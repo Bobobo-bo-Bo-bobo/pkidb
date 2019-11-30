f1fe0e4effdcee1b1f389eddb69199e649cc65af
Andreas Maus <maus@badphish.ypbind.de>
2019-11-30 18:26:45 +0100

    Add mapping for OpenSSL dgst name to Golang x509.SignatureAlgorithm

--- src/pkidb/constants.go
+++ src/pkidb/constants.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"crypto/x509"
+)
+
 const name string = "pkidb"
 const version string = "1.0.0-2019.11.30"
+
+// DigestMap - Map OpenSSL digest to Golang x509.SignatureAlgorithm
+var DigestMap = map[string]x509.SignatureAlgorithm{
+	"md5":    x509.MD5WithRSA,
+	"sha1":   x509.SHA1WithRSA,
+	"sha256": x509.SHA256WithRSA,
+	"sha384": x509.SHA384WithRSA,
+	"sha512": x509.SHA512WithRSA,
+}
