5e2f5f411bd588a1299293fa86463f880beec92f
Andreas Maus <maus@badphish.ypbind.de>
2019-11-30 19:15:40 +0100

    Maximal serial number as allowed by RFC 3280

--- src/pkidb/constants.go
+++ src/pkidb/constants.go
@@ -15,3 +15,30 @@ var DigestMap = map[string]x509.SignatureAlgorithm{
 	"sha384": x509.SHA384WithRSA,
 	"sha512": x509.SHA512WithRSA,
 }
+
+/*
+  Note: RFC 3280 - 4.1.2.2  Serial number (see https://www.ietf.org/rfc/rfc3280.txt) states:
+
+4.1.2.2  Serial number
+
+   The serial number MUST be a positive integer assigned by the CA to
+   each certificate.  It MUST be unique for each certificate issued by a
+   given CA (i.e., the issuer name and serial number identify a unique
+   certificate).  CAs MUST force the serialNumber to be a non-negative
+   integer.
+
+   Given the uniqueness requirements above, serial numbers can be
+   expected to contain long integers.  Certificate users MUST be able to
+   handle serialNumber values up to 20 octets.  Conformant CAs MUST NOT
+   use serialNumber values longer than 20 octets.
+
+   Note: Non-conforming CAs may issue certificates with serial numbers
+   that are negative, or zero.  Certificate users SHOULD be prepared to
+   gracefully handle such certificates.
+
+
+ -> so 0x7fffffffffffffff is the maximum serial number because of "Certificate users MUST be able to
+    handle serialNumber values up to 20 octets"
+
+*/
+const MaximumSerialNumberString = "0x7fffffffffffffff"
