7295e4f1f2afbbcc578fffabb04103354b7676ac
Andreas Maus <maus@badphish.ypbind.de>
2019-12-17 11:26:17 +0100

    Ooops wrong branch

--- src/pkidb/constants.go
+++ src/pkidb/constants.go
@@ -7,7 +7,7 @@ import (
 )
 
 const name = "pkidb"
-const version = "1.0.1-2019.12.16"
+const version = "1.0.0"
 
 // DummyCertificateSubject - subject for dummy entry
 const DummyCertificateSubject = "dummy entry"
